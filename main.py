c0060fe754debdfd6497d4ef8dca4bd1b392b7b5
botuser <s.chladek@gmx.de>
2020-11-26 10:38:48 +0100

    First_upload

---
+++ main.py
@@ -0,0 +1,27 @@
+# Importieren der Pygame-Bibliothek
+import pygame
+
+# initialisieren von pygame
+pygame.init()
+
+# genutzte Farbe
+ORANGE  = ( 255, 140, 0)
+ROT     = ( 255, 0, 0)
+GRUEN   = ( 0, 255, 0)
+SCHWARZ = ( 0, 0, 0)
+WEiSS   = ( 255, 255, 255)
+
+# Fenster öffnen
+pygame.display.set_mode((640, 480))
+
+# Titel für Fensterkopf
+pygame.display.set_caption("Unser erstes Pygame-Spiel")
+
+# solange die Variable True ist, soll das Spiel laufen
+spielaktiv = True
+
+# Schleife Hauptprogramm
+while spielaktiv:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            spielaktiv = False
